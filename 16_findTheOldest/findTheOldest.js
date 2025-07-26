5a74db9f701008d28e2342b7bb51f84fe2031619
VincECodesNicely <vk6953@hotmail.com>
2025-07-26 13:56:25 +1200

    finsihed the exercises

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,16 @@
-const findTheOldest = function() {
+const getAge = function (birth, death) {
+  if (!death) {
+    death = new Date().getFullYear();
+  }
+  return death - birth;
+};
 
+const findTheOldest = function (people) {
+  return people.reduce((old, current) => {
+    const lastPerson = getAge(old.yearOfBirth, old.yearOfDeath);
+    const currentPerson = getAge(current.yearOfBirth, current.yearOfDeath);
+    return lastPerson < currentPerson ? current : old;
+  });
 };
 
 // Do not edit below this line
